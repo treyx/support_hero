b9265e3e1ed7a064f9e6229c55dd44364036379d
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-06-06 14:53:14 -0600

    cleaned feature test up a bit and added test for displaying todays hero

--- spec/features/unauthenticated_user_spec.rb
+++ spec/features/unauthenticated_user_spec.rb
@@ -1,10 +1,17 @@
 require "rails_helper"
 
 describe "the unauthenticated user", type: :feature do
+  let!(:user) { User.create(name: "username") }
+  let!(:shift) { Shift.create(date: Date.today, user_id: user.id) }
 
-  it "can see today's schedule from the root" do
+  scenario "can see today's schedule from the root" do
     visit root_path
-    expect(page).to have_content("#{Date.today.strftime("%m/%d/%Y")}")
+    expect(page).to have_content("#{Date.today}")
     expect(page).to have_content("Today's Scheduled Support Hero:")
+  end
+
+  scenario "can see the name of the Hero who is scheduled today" do
+    visit root_path
+    expect(page).to have_content(user.name)
   end
 end
