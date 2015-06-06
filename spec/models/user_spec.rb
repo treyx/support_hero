5f008e322da061390c60b8c49b86d437eed49823
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-06-06 13:45:09 -0600

    added expectation for User model spec - added has_many relationship for User-

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,4 +9,15 @@ RSpec.describe User, :type => :model do
       expect(user).to_not be_valid
     end
   end
+
+  describe "relationships" do
+    let!(:shift_1) { Shift.create(date: Date.today, user_id: user.id) }
+    let!(:shift_2) { Shift.create(date: Date.tomorrow, user_id: user.id) }
+
+    it "can have many shifts" do
+      expect(user.shifts).to include(shift_1)
+      expect(user.shifts).to include(shift_2)
+      expect(user.shifts.count).to eq(2)
+    end
+  end
 end
