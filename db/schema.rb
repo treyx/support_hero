45c19113c2739a9d0d2728c2b9f356de14449d8e
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-06-06 12:53:04 -0600

    added user migration with name and timestamps

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150606180149) do
+ActiveRecord::Schema.define(version: 20150606185146) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "shifts", force: :cascade do |t|
     t.string   "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
