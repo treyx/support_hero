0837f6db7175d82f092f8881fe89dff141c24d38
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-06-06 17:06:10 -0600

    spec for login_path

--- spec/features/authentication_spec.rb
+++ spec/features/authentication_spec.rb
@@ -5,8 +5,15 @@ describe "the application", type: :feature do
   context "when logged out" do
     before(:each) { visit root_path }
 
-    it "has a login button" do
+    scenario "has a login button" do
       expect(page).to have_link("Log In")
+    end
+
+    scenario 'can visit a login page' do
+      visit login_path
+      expect(page).to have_field("name")
+      expect(page).to have_field("password")
+      expect(page).to have_button("Login!")
     end
   end
 end
