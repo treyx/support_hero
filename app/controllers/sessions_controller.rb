3313ccc614bd0f2db67cbeb36073f3e5520b2a93
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-06-06 21:23:22 -0600

    finished logout functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,4 +13,10 @@ class SessionsController < ApplicationController
       redirect_to :back
     end
   end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "See you next time!"
+    redirect_to root_path
+  end
 end
