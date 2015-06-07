814839200eddd13ffef07047abfe6372dbe1b175
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-06-06 21:47:03 -0600

    added button to view specific schedule and nested it in current user conditional

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   get "/login",  to: "sessions#new"
   post "/login", to: "sessions#create"
   get "/logout", to: "sessions#destroy"
+
+  resources :users, only: [:show]
 end
