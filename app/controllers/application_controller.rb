fe415dd62b089022d2df63869b0e8f4b08bd1581
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-06-07 10:21:59 -0600

    added some more conditional logic to the navbar and a few helper methods to keep it clean

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  helper_method :todays_hero, :current_user
+  helper_method :todays_hero, :current_user, :current_user_is_hero, :current_user_is_admin
 
   def todays_hero
     todays_shift = Shift.find_by(date: Date.today)
@@ -12,5 +12,13 @@ class ApplicationController < ActionController::Base
 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def current_user_is_hero
+    current_user && current_user.hero?
+  end
+
+  def current_user_is_admin
+    current_user && current_user.admin?
   end
 end
