7014b0e3e58fe6434164a31402cfeea478bb6d8e
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-06-07 17:11:40 -0600

    added button to remove shift and updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   get "/logout", to: "sessions#destroy"
 
   resources :users, only: [:show]
-  resources :shifts, only: [:new, :create]
+  resources :shifts, only: [:new, :create, :update]
 end
