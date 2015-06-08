fd1c3cb8da0e891b51666612fc5dfda75ea15e01
Trey Tomlinson <tomlinson.trey@gmail.com>
2015-06-07 18:34:18 -0600

    added logic for selection collection and fixed methods for updated helper method

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -10,8 +10,14 @@ class ShiftsController < ApplicationController
   end
 
   def update
-    shift = Shift.find(params[:id])
-    shift.update_attributes(user_id: select_id_from_available_user)
+    if params[:commit] == "Swap!"
+      shift = Shift.find(params[:id])
+      shift.update_attributes(user_id: params[:shift][:user_id])
+    else
+      shift = Shift.find(params[:id])
+      shift.update_attributes(user_id: all_users_except_current.sample.id)
+    end
+
     flash[:success] = "Enjoy your day off!"
     redirect_to :back
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def show
     @shifts = Shift.where(user_id: current_user.id)
+    @heroes = all_users_except_current
   end
 end
